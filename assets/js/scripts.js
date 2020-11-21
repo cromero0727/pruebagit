d8c13e088db278d285866cda1935d15cde7f2903
Cesar Romero <cromero@planok.com>
2020-11-21 19:08:23 -0300

    modal mediaquerys y estilos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+// SMOOTH SCROLL 
 $(function() {
 
     $("a").click(function(event) {
@@ -13,31 +14,18 @@ $(function() {
         }
     });
 });
+// TOOLTIP 
 $(function() {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
-// var cambio = document.getElementById("cambio");
-// var seccionquiensoy = document.getElementById("quiensoy");
-// var header = document.getElementById("header-cr");
+// POP OVER 
+$(function() {
+    $('[data-toggle="popover"]').popover()
+})
 
-
-// cambio.addEventListener("click", function() {
-//     seccionquiensoy.classList.add('cambiocolor');
-// });
-
-
-
-
-
-
-// var parrafoDos = document.getElementById("par-2");
-// var parrafoCuatro = document.getElementById("par-4");
-// var parrafoTres = document.getElementById("par-3");
-
-// parrafoDos.addEventListener("click", function() {
-//     parrafoCuatro.classList.toggle("good");
-// });
-// parrafoTres.addEventListener("click", function() {
-//     parrafoCuatro.classList.toggle("succes");
-// });
+$(function() {
+    $('.example-popover').popover({
+        container: 'body'
+    })
+})
